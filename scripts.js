5745a636fa4cfde8ce7d2ab4e3805970e003d1cc
viettien1404 <leviettien.n1404@n1404gmail.n1404gmailcom>
2025-06-14 23:08:39 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,448 @@
+// Dữ liệu cho tab Học Sinh
+const hocSinhData = [
+  {
+    chude:
+      "CHỦ ĐỀ A. MÁY TÍNH VÀ XÃ HỘI TRI THỨC - GIỚI THIỆU TRÍ TUỆ NHÂN TẠO",
+    bai: [
+      {
+        ten: "Bài 1: Giới thiệu về trí tuệ nhân tạo",
+
+        form: "https://forms.gle/8MCEuoQ5PEftG4Db7",
+      },
+      {
+        ten: "Bài 2: Giới thiệu về trí tuệ nhân tạo (tiếp theo)",
+
+        form: "https://forms.gle/KkhpLRJAD5F2QvoW9",
+      },
+    ],
+  },
+  {
+    chude: "CHỦ ĐỀ B. MẠNG MÁY TÍNH VÀ INTERNET - KẾT NỐI MẠNG",
+    bai: [
+      {
+        ten: "Bài 1: Cơ sở về mạng máy tính",
+
+        form: "https://forms.gle/qYuhhh4Dwnq6EYbeA",
+      },
+      {
+        ten: "Bài 2: Các giao thức mạng",
+
+        form: "https://forms.gle/LgvgshDqdRi25EUP6",
+      },
+      {
+        ten: "Bài 3: Thực hành thiết lập kết nối và sử dụng mạng",
+
+        form: "https://forms.gle/t9xYrX5jz2nhxYwE6",
+      },
+    ],
+  },
+  {
+    chude: "CHỦ ĐỀ D. ĐẠO ĐỨC, PHÁP LUẬT VÀ VĂN HOÁ TRONG MÔI TRƯỜNG SỐ",
+    bai: [
+      {
+        ten: "Bài 1: Giao tiếp và tính nhân văn trong ứng xử trên không gian mạng",
+
+        form: "https://forms.gle/NxndKRHtFvMyAceh6",
+      },
+    ],
+  },
+  {
+    chude:
+      "CHỦ ĐỀ Aict. MÁY TÍNH VÀ XÃ HỘI TRI THỨC - THỰC HÀNH KẾT NỐI THIẾT BỊ SỐ",
+    bai: [
+      {
+        ten: "Bài 1: Thực hành kết nối máy tính với ti vi thông minh",
+
+        form: "https://forms.gle/npWUBCwYZNzZYvPU7",
+      },
+      {
+        ten: "Bài 2: Thực hành theo nhóm: Kết nối các thiết bị không dây cho ứng dụng",
+
+        form: "https://forms.gle/zYS4JAmuRUAfVhVm7",
+      },
+    ],
+  },
+  {
+    chude:
+      "CHỦ ĐỀ F. GIẢI QUYẾT VẤN ĐỀ VỚI SỰ TRỢ GIÚP CỦA MÁY TÍNH - TẠO TRANG WEB",
+    bai: [
+      {
+        ten: "Bài 1: Làm quen với ngôn ngữ đánh dấu siêu văn bản",
+
+        form: "https://forms.gle/wK4yDXQr9LN6HBxy7",
+      },
+      {
+        ten: "Bài 2: Định dạng văn bản và tạo siêu liên kết",
+
+        form: "https://forms.gle/HoB1EZvfaCa7C5EU8",
+      },
+      {
+        ten: "Bài 3: Thực hành định dạng văn bản và tạo siêu liên kết",
+
+        form: "https://forms.gle/hREgFbgiyrDZFCL79",
+      },
+      {
+        ten: "Bài 4: Trình bày nội dung theo dạng danh sách, bảng biểu",
+
+        form: "https://forms.gle/jbCyGtao1BtKmcih6",
+      },
+      {
+        ten: "Bài 5: Chèn hình ảnh, âm thanh, video và sử dụng khung",
+
+        form: "https://forms.gle/4b7FoK3rFe7a6dof8",
+      },
+      {
+        ten: "Bài 6: Tạo biểu mẫu",
+
+        form: "https://forms.gle/Gu21zVQxEMNyn5LbA",
+      },
+      {
+        ten: "Bài 7: Thực hành tạo biểu mẫu",
+
+        form: "https://forms.gle/eUyG9PHxhDF1aNFR9",
+      },
+      {
+        ten: "Bài 8: Làm quen với CSS",
+
+        form: "https://forms.gle/uoY8P355oD2YXeGZA",
+      },
+      {
+        ten: "Bài 9: Thực hành định dạng một số thuộc tính CSS",
+
+        form: "https://forms.gle/oAzmGQAFhdxcWjGEA",
+      },
+      {
+        ten: "Bài 10: Bộ chọn lớp, bộ chọn định danh",
+
+        form: "https://forms.gle/o1TnWaVHZ3fnRB4u8",
+      },
+      {
+        ten: "Bài 11: Mô hình hộp, bố cục trang web",
+
+        form: "https://forms.gle/gfLRTwoh2SRoZeE37",
+      },
+      {
+        ten: "Bài 12: Dự án nhỏ: Tạo trang web báo tường",
+
+        form: "https://forms.gle/JccxzuNhRo78tB5i9",
+      },
+    ],
+  },
+  {
+    chude:
+      "CHỦ ĐỀ G. HƯỚNG NGHIỆP VỚI TIN HỌC - GIỚI THIỆU NHÓM NGHỀ DỊCH VỤ VÀ QUẢN TRỊ, MỘT SỐ NGHỀ ỨNG DỤNG TIN HỌC VÀ MỘT SỐ NGÀNH THUỘC LĨNH VỰC TIN HỌC",
+    bai: [
+      {
+        ten: "Bài 1: Giới thiệu nhóm nghề Dịch vụ và Quản trị trong ngành Công nghệ thông tin",
+
+        form: "https://forms.gle/KsaRa1JYnVPBjgtN8",
+      },
+      {
+        ten: "Bài 2: Một số nghề khác trong ngành Công nghệ thông tin và một số nghề ứng dụng công nghệ thông tin",
+
+        form: "https://forms.gle/hsHVajHXEWvVhrFV6",
+      },
+      {
+        ten: "Bài 3: Dự án nhỏ: Tìm hiểu thông tin tuyển sinh và hướng nghiệp về lĩnh vực đào tạo 'Máy tính và Công nghệ thông tin' ở Việt Nam",
+
+        form: "https://forms.gle/PLQUvrE2MxpdjMdF6",
+      },
+    ],
+  },
+  {
+    chude:
+      "CHỦ ĐỀ Eict. ỨNG DỤNG TIN HỌC - THỰC HÀNH SỬ DỤNG PHẦN MỀM TẠO TRANG WEB",
+    bai: [
+      {
+        ten: "Bài 1: Giới thiệu phần mềm tạo website",
+
+        form: "https://forms.gle/hz7vLECzcthRgwru7",
+      },
+      {
+        ten: "Bài 2: Tạo website bằng phần mềm",
+
+        form: "https://forms.gle/chNfD2zVgVSEHjZQ9",
+      },
+      {
+        ten: "Bài 3: Tạo thanh điều hướng cho trang web",
+
+        form: "https://forms.gle/hj9EYpHLtTNCftYJ8",
+      },
+      {
+        ten: "Bài 4: Tạo nội dung văn bản cho trang web",
+
+        form: "https://forms.gle/XthcLBTubKpzR4Dd6",
+      },
+      {
+        ten: "Bài 5: Tạo nội dung hình ảnh cho trang web",
+
+        form: "https://forms.gle/AzqkxZG8b5Zmi8Rx9",
+      },
+      {
+        ten: "Bài 6: Tạo biểu mẫu cho trang web và xuất bản website",
+
+        form: "https://forms.gle/TamXdrDc1WFGkRU39",
+      },
+      {
+        ten: "Bài 7: Tạo sản phẩm theo nhóm (Bài tập nhóm)",
+
+        form: "https://forms.gle/ZSR964XcXd5VwYSN6",
+      },
+    ],
+  },
+
+  {
+    chude: "MỘT SỐ ĐỀ THI TRƯỜNG SỞ MÔN TIN HỌC ",
+    bai: [
+      {
+        ten: "Đề 1",
+        pdf: "https://drive.google.com/file/d/12yHvarjfR5OjzlR66yOzMb0lvkTslMRZ/view?usp=drivesdk",
+      },
+    ],
+  },
+
+  {
+    chude: "MỘT SỐ ĐỀ THI THỬ TỐT NGHIỆP THPT MÔN TIN HỌC",
+    bai: [
+      {
+        ten: "Đề 1",
+
+        form: "https://forms.gle/Hc8sqVWKnVLDU2p16",
+      },
+      {
+        ten: "Đề 2",
+
+        form: "https://forms.gle/BhyR9noSVGyB9fvT7",
+      },
+      {
+        ten: "Đề 3",
+
+        form: "https://forms.gle/ZdP4aGjsGeFU44rGA",
+      },
+      {
+        ten: "Đề 4",
+
+        form: "https://forms.gle/w49AyfxknoCLcVTK8",
+      },
+      {
+        ten: "Đề 5",
+
+        form: "https://forms.gle/LJDBwv8C6J4vZfH19",
+      },
+
+      {
+        ten: "Đề 6",
+        pdf: "https://drive.google.com/file/d/12yHvarjfR5OjzlR66yOzMb0lvkTslMRZ/view?usp=drivesdk",
+      },
+    ],
+  },
+];
+
+// Dữ liệu cho tab "Các Văn Bản Chỉ Đạo"
+const vanBanData = [
+  {
+    chude: "Một số văn bản chỉ đạo và kế hoạch:",
+    bai: [
+      {
+        ten: "Thông tư 32 của Bộ GDĐT về Chương trình GDPT 2018 (Chương trình tổng thể).",
+        pdf: "https://drive.google.com/file/d/1sxdT4JMqlW56XaxNzajNcw9UQ_UYGjC3/view?usp=sharing",
+      },
+      {
+        ten: "Thông tư 32 của Bộ GDĐT về Chương trình GDPT 2018 (Chương trình các môn học/Hoạt động giáo dục).",
+        pdf: "https://drive.google.com/file/d/1U54lJBNvdn2nooetcMPm7fWyz-HovrTx/view?usp=sharing",
+      },
+      {
+        ten: "Quyết định số 764/QĐ-BGDĐT ngày 08/3/2024 quy định về cấu trúc định dạng đề thi kỳ thi tốt nghiệp THPT từ năm 2025.",
+        pdf: "https://drive.google.com/file/d/1MejixVruKXallzb-BTuD_eWyqDuHongu/view?usp=sharing",
+      },
+      {
+        ten: "Tài liệu tập huấn xây dựng câu hỏi thi tốt nghiệp THPT từ 2025 môn Tin học.",
+        pdf: "https://drive.google.com/file/d/1JMqQXO4pRn3p7JN3cb4hsbUt6iESTjAF/view?usp=sharing",
+      },
+      {
+        ten: "Hướng dẫn ôn thi tốt nghiệp môn Tin Học ",
+        pdf: "https://drive.google.com/file/d/13BiaGihTUDbXLALu97oxPXlMBkTqhC0v/view?usp=drivesdk",
+      },
+    ],
+  },
+  {
+    chude: "Đề minh họa thi tốt nghiệp từ 2025 - Công bố ngày 29/12/2023",
+    bai: [
+      {
+        ten: "Đề minh họa:",
+        pdf: "https://drive.google.com/file/d/1AP1o-GkaDz6xtxZAlEEwA3pxPT7K0BrH/view?usp=sharing",
+      },
+      {
+        ten: "Đáp án:",
+        pdf: "https://drive.google.com/file/d/19PxKXYXFEyTh5Vg-aYRSqNdMh-49iWtX/view?usp=sharing",
+      },
+    ],
+  },
+];
+
+// Dữ liệu cho tab Giáo Viên
+const giaoVienData = [
+  {
+    chude:
+      "CHỦ ĐỀ A. MÁY TÍNH VÀ XÃ HỘI TRI THỨC - GIỚI THIỆU TRÍ TUỆ NHÂN TẠO",
+    bai: [
+      {
+        ten: "Bài 1: Giới thiệu về trí tuệ nhân tạo",
+        youtube: "https://youtu.be/5jo1HL2EzD0",
+      },
+      {
+        ten: "Bài 2: Giới thiệu về trí tuệ nhân tạo (tiếp theo)",
+        youtube: "https://youtu.be/9KrLLkl-4Rg",
+      },
+    ],
+  },
+  {
+    chude: "CHỦ ĐỀ B. MẠNG MÁY TÍNH VÀ INTERNET - KẾT NỐI MẠNG",
+    bai: [
+      {
+        ten: "Bài 1: Cơ sở về mạng máy tính",
+        youtube: "https://youtu.be/2ZCYULHKLv8",
+      },
+      {
+        ten: "Bài 2: Các giao thức mạng",
+        youtube: "https://youtu.be/5usvtVUZm-o",
+      },
+    ],
+  },
+  {
+    chude:
+      "CHỦ ĐỀ F. GIẢI QUYẾT VẤN ĐỀ VỚI SỰ TRỢ GIÚP CỦA MÁY TÍNH - TẠO TRANG WEB",
+    bai: [
+      {
+        ten: "Bài 1: Làm quen với ngôn ngữ đánh dấu siêu văn bản",
+        youtube: "https://youtu.be/OyTm0Kgq6ts",
+      },
+      {
+        ten: "Bài 2: Định dạng văn bản và tạo siêu liên kết",
+        youtube: "https://youtu.be/p-rATHcn46Q",
+      },
+      {
+        ten: "Bài 3: Thực hành định dạng văn bản và tạo siêu liên kết",
+        youtube: "https://youtu.be/Cv1BjITOdgk",
+      },
+      {
+        ten: "Bài 4: Trình bày nội dung theo dạng danh sách, bảng biểu",
+        youtube: "https://youtu.be/tl9N1v6mXi4",
+      },
+      {
+        ten: "Bài 5: Chèn hình ảnh, âm thanh, video và sử dụng khung",
+        youtube: "https://youtu.be/00YpHKX2x7Q",
+      },
+      { ten: "Bài 6: Tạo biểu mẫu", youtube: "https://youtu.be/2uL0wW5p3vM" },
+      {
+        ten: "Bài 7: Thực hành tạo biểu mẫu",
+        youtube: "https://youtu.be/CYusuaMbsbo",
+      },
+      {
+        ten: "Bài 8: Làm quen với CSS",
+        youtube: "https://youtu.be/3lNH19cXSrc",
+      },
+      {
+        ten: "Bài 9: Thực hành định dạng một số thuộc tính CSS",
+        youtube: "https://youtu.be/VYtBefnGtjM",
+      },
+      {
+        ten: "Bài 10: Bộ chọn lớp, bộ chọn định danh",
+        youtube: "https://youtu.be/7_aGz0tCRSQ",
+      },
+      {
+        ten: "Bài 11: Mô hình hộp, bố cục trang web",
+        youtube: "https://youtu.be/c5TOhsszbdM",
+      },
+      {
+        ten: "Bài 12: Dự án nhỏ: Tạo trang web báo tường",
+        youtube: "https://youtu.be/zVx5nzcNiCM",
+      },
+    ],
+  },
+];
+
+function generateAccordion(data, containerId) {
+  const container = document.getElementById(containerId);
+  container.innerHTML = "";
+
+  data.forEach((chude, index) => {
+    const accordion = document.createElement("div");
+    accordion.className = "accordion";
+
+    const header = document.createElement("div");
+    header.className = "accordion-header";
+    header.innerHTML = `${chude.chude} <span class="toggle-icon">+</span>`;
+    header.onclick = () => toggleContent(index, header, containerId);
+
+    const content = document.createElement("div");
+    content.className = "accordion-content";
+    content.style.display = "none";
+
+    const ul = document.createElement("ul");
+    chude.bai.forEach((bai) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${bai.ten}`;
+      if (bai.pdf) {
+        li.innerHTML += ` - <a href="${bai.pdf}" target="_blank">Tải bản pdf miễn phí</a>`;
+      }
+      if (bai.form) {
+        li.innerHTML += ` - <a href="${bai.form}" target="_blank">Kiểm tra thử miễn phí</a>`;
+      }
+      if (bai.youtube) {
+        li.innerHTML += ` - <a href="${bai.youtube}" target="_blank">Link Youtube</a>`;
+      }
+      ul.appendChild(li);
+    });
+    content.appendChild(ul);
+
+    accordion.appendChild(header);
+    accordion.appendChild(content);
+    container.appendChild(accordion);
+  });
+}
+
+function toggleContent(index, header, containerId) {
+  const contents = document.querySelectorAll(
+    `#${containerId} .accordion-content`
+  );
+  const icons = document.querySelectorAll(`#${containerId} .toggle-icon`);
+
+  const content = contents[index];
+  const icon = icons[index];
+
+  if (content.style.display === "none") {
+    content.style.display = "block";
+    icon.innerText = "−";
+  } else {
+    content.style.display = "none";
+    icon.innerText = "+";
+  }
+
+  header.classList.toggle("active");
+}
+
+function openTab(tabId) {
+  document.querySelectorAll(".tab-content").forEach((content) => {
+    content.style.display = "none";
+  });
+  document.getElementById(tabId).style.display = "block";
+
+  document.querySelectorAll(".tab-button").forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  document
+    .querySelector(`button[onclick="openTab('${tabId}')"]`)
+    .classList.add("active");
+
+  if (tabId === "hoc-sinh") {
+    generateAccordion(hocSinhData, "hoc-sinh-container");
+  } else if (tabId === "giao-vien") {
+    generateAccordion(giaoVienData, "giao-vien-container");
+  } else if (tabId === "van-ban") {
+    generateAccordion(vanBanData, "van-ban-container");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => openTab("hoc-sinh"));
